ecac572eaabf09ac3d10df4866d8ed1ae3a64644
Hong9915 <hhj4498@naver.com>
2025-10-15 14:29:33 +0900

    동적

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
-// next.config.ts
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",          // ★ 정적 산출물 내보내기
-  images: { unoptimized: true }, // (이미지 최적화 비활성화)
-  // basePath / assetPrefix 쓰면 out/ 경로가 달라지니 주의
+const nextConfig = {
+  output: 'standalone', // 또는 아예 속성 제거
 };
 export default nextConfig;
